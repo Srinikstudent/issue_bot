7ea93abc6c445c74e74e7f8435d27b865de721dd
srini_kachavarapu <105570161+Srinikstudent@users.noreply.github.com>
2022-08-19 21:24:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,62 +24,7 @@ def predict_label(issue_id, title, body, is_test):
    
     X = [title] + [body]
     label = model.predict(X)
-    print('issue_id is ' + issue_id + ' label is ' + label )
-    print (is_test)
-    #saving incoming data 
-    mylist = [] 
-    # for is_test in (issue_id, title, body, label[0]):
-    data = [issue_id, title, body, label[0]]
-    mylist.append(data)
-   # if len(mylist) == 1000:
-   #header =['issue_id','title','body','predicted_label']
-    data = mylist
-    with open('pred_label_data.csv', 'a' ,encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-
-    return label[0]        
-    
-   
-   
-
-def correct_label(issue_id, correct_label):
-    """
-        This is the correction function, you should count correction toward your
-        success measurement as well as use it to correct your available data.
-    """
-    print('issue_id is ' + issue_id + ' corrected label is ' + correct_label)
-    mylist = [] 
-  # for data in (issue_id, title, body, label[0]):
-    data = [issue_id, correct_label]
-    mylist.append(data)
-   # if len(mylist) == 1000:
-   #header = ['issue_id','corredted label']
-    data = mylist
-    with open('corrected_label_data.csv', 'a',encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-
-    
-    # save metrics here
-
-@app.route('/metrics', methods=['GET'])
-def metrics():
-    """
-        This is the metrics endpoint that gets called from prometheus.
-    """
-    df1 = pd.read_csv('pred_label_data.csv')
-    df2 = pd.read_csv('corrected_label_data.csv')
-    
-    num_requests = len(df1)
-    num_label_corrections = len(df2)
-    
-    # end load metrics
-
-    metrics = ''
-    metrics += f'num_requests{{env="{flask_env}"}} {num_requests}\n'
-    metrics += f'num_label_corrections{{env="{flask_env}"}} {num_label_corrections}\n'
-    return metrics
+    return label[0]             
 
 @app.route('/form', methods=['GET'])
 def form():
@@ -91,7 +36,7 @@ def form():
         port = 5001
     if flask_env == 'staging':
         port = 5002
-    return render_template("form.html", version=version, flask_env=flask_env, correction_url=f'http://127.0.0.1:{port}/api/correction', issue_url=f'http://127.0.0.1:{port}/api/issue')
+    return render_template("form.html", version=version, flask_env=flask_env, correction_url=f'https://gt-srini-bot.herokuapp.com/api/correction', issue_url=f'https://gt-srini-bot.herokuapp.com/api/issue')
 
 @app.route('/api/issue', methods=['POST'])
 def predict_issue_label():
